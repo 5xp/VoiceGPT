a71383b34782e5260958c494c77920e45735403f
5xp <62446202+5xp@users.noreply.github.com>
2023-01-04 17:40:14 -0600

    Remove join by ID option.

--- src/commands/join.js
+++ src/commands/join.js
@@ -5,20 +5,9 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("join")
     .setDescription("Joins the voice channel you are in.")
-    .setDMPermission(false)
-    .addStringOption(option => option.setName("id").setDescription("Voice channel ID to join.").setRequired(false)),
+    .setDMPermission(false),
   async execute(interaction) {
-    let voiceChannel;
-
-    const channelId = interaction.options.getString("id");
-    if (channelId) {
-      const channel = interaction.client.channels.cache.get(channelId);
-      if (channel.isVoiceBased?.()) {
-        voiceChannel = channel;
-      }
-    } else {
-      voiceChannel = interaction.member.voice.channel;
-    }
+    const voiceChannel = interaction.member.voice.channel;
 
     if (!voiceChannel) {
       return interaction.reply({ content: "You need to be in a voice channel to use this command.", ephemeral: true });
@@ -29,7 +18,6 @@ module.exports = {
     try {
       const voiceClient = new VoiceClient(interaction.client);
       await voiceClient.connectToChannel(voiceChannel);
-      console.log("🔊 Joined voice channel.");
     } catch (error) {
       console.warn(error);
       return interaction.followUp("Failed to join voice channel within 20 seconds, please try again later.");
